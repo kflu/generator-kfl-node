886848440efe6ddd1175f9306e98721d7f55774a
KFL <kfldev@outlook.com>
2016-05-11 13:56:32 -0700

    rename .gitignore to gitignore as npm publish doesn't like the former

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,8 +23,8 @@ module.exports = yeoman.Base.extend({
       this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), {name: this.props.name});
       this.fs.copy(this.templatePath('.babelrc'), this.destinationPath('.babelrc'));
       this.fs.copy(this.templatePath('.eslintrc.json'), this.destinationPath('.eslintrc.json'));
-      this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
-      this.fs.copy(this.templatePath('src/index.js'), this.destinationPath('src/index.js'));
+      this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
+      this.fs.copy(this.templatePath('src/index.js'), this.destinationPath('src/' + this.props.name + '.js'));
   },
 
   install: function () {
